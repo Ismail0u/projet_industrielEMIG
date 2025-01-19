a87f8e1b87f62df75dafde8f0bfa0df33c134609
Tasnim Lawan <tasnim1177@gmail.com>
2025-01-19 23:59:58 +0100

    Ajout de la page de connexion avec formulaire React

--- monFrontend/frontend/src/App.jsx
+++ monFrontend/frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+<<<<<<< HEAD
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +31,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+=======
+     <BrowserRouter>
+            <Routes>
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
+>>>>>>> 0643efc (Ajout de la page de connexion avec formulaire React)
     </>
   )
 }
